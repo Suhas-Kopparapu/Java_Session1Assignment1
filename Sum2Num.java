f2ea40fb9491781e3d098332130c319533f41270
Suhas-Kopparapu <kopparapu.suhas@gmail.com>
2016-07-18 19:54:33 +0530

    acad@session1.ass1upload

---
+++ Sum2Num.java
@@ -0,0 +1,15 @@
+package Assignments;
+
+public class Sum2Num {
+	
+	public  static void main(String[] args){
+		int num1=0;
+		float num2 =500;
+		int sum = 0;
+		
+		sum = num1 + (int)num2;
+		System.out.println("The sum of the numbers is "+sum);
+		
+	}
+
+}
